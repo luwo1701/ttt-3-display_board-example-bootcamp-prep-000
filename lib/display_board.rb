9cb1f3446860db73fd277911d2d84b8b425bc552
Luke Worley <luke.worley@colorado.edu>
2018-05-20 15:59:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "A Tic Tac Toe Board"
+  #puts "A Tic Tac Toe Board"
   for i in 1..5
     puts i
     if i%2 == 0
